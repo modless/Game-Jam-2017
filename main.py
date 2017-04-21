b487bb6168373dc1381f268ace707cbe5dc2e4cb
Modless <ryrius@gmail.com>
2017-04-22 00:30:28 +0300

    .ignore and button sounds

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ kivy.require('1.9.0')
 
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.widget import Widget
+from kivy.graphics import *
+from kivy.core.audio import SoundLoader
+
+sound = SoundLoader.load('Untitled 2.wav')
 
 class RootWidget(FloatLayout):
     pass
