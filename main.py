d853fbb36157ac58d7b858477a188600fcedf2d3
Modless <ryrius@gmail.com>
2017-04-22 01:58:11 +0300

    Pull

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.widget import Widget
 from kivy.graphics import *
 from kivy.core.audio import SoundLoader
+from kivy.uix.image import Image
+from kivy.graphics.svg import Svg
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
 
 sound = SoundLoader.load('Untitled 2.wav')
 
