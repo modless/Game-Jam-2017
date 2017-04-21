ed2e13cbbc583fc5704e04ac89ea5a07f6ef39b6
SetAsForMatt <matthew.ladyga@gmail.com>
2017-04-22 01:57:21 +0300

    wins ale mapp also

--- main.py
+++ main.py
@@ -5,9 +5,23 @@ from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.widget import Widget
 from kivy.graphics import *
-from kivy.core.audio import SoundLoader
+from kivy.core.audio import SoundLoader # garsam
+from kivy.uix.screenmanager import ScreenManager, Screen # ekranam
 
-sound = SoundLoader.load('Untitled 2.wav')
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++=
+
+cliclSound = SoundLoader.load('Untitled 2.wav') # uzloadinam click garsa
+screenMng = ScreenManager() # screen manageris
+
+screenMng.add_widget(MapScreen(name='mapScreen'))
+screenMng.add_widget(StationsScreen(name='stationsScreen'))
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+class MapScreen(Screen):
+    pass
+class StationsScreen(Screen):
+    pass
+
 
 class RootWidget(FloatLayout):
     pass
@@ -16,6 +30,8 @@ class MainApp(App):
 
     def build(self):
         return RootWidget()
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 if __name__ == '__main__':
     MainApp().run()
