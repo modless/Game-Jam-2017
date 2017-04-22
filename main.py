46b7328e3fff5c9718c9f131e10dd7c37ca794ce
Modless <ryrius@gmail.com>
2017-04-22 03:46:18 +0300

    bug fix

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 import kivy
 kivy.require('1.9.0')
 
+import sys
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.widget import Widget
 from kivy.graphics import *
-from kivy.core.audio import SoundLoader
 from kivy.uix.image import Image
-from kivy.graphics.svg import Svg
 from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
 from kivy.core.audio import SoundLoader # garsam
-from kivy.uix.screenmanager import ScreenManager, Screen # ekranam
 
-map = Image(source='map.png')
 class RootWidget(FloatLayout):
     pass
 
