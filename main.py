c90a2a2a888c99ae82ee52e56a53284890be0f2b
Modless <ryrius@gmail.com>
2017-04-22 14:13:25 +0300

    products

--- main.py
+++ main.py
@@ -16,17 +16,42 @@ from kivy.properties import StringProperty, NumericProperty
 from kivy.uix.label import Label
 import time
 
-
-
 class RootWidget(FloatLayout):
     pass
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 class MainApp(App):
-    cash = 1000
+    cash = 100000
     cashStr = StringProperty("'Money:' + str(self.cash) + '\u20ac'")
     income = NumericProperty(0)
+
+    ##########################
+    #   Product Object List  #
+    ##########################
+    #Product(1000, 0)
+    #Shrooms = Product(2000, 15, 0)
+    #Heroin = Product(5000, 50, 0)
+    #Stomachs = Product(7000, 75, 0)
+    Products = {
+        "Marijuana": {
+            "cost": 1000,
+            "quantity": 0,
+        },
+        "Shrooms": {
+            "cost": 2000,
+            "quantity": 0
+        },
+        "Heroin": {
+            "cost": 5000,
+            "quantity": 0
+        },
+        "Stomachs": {
+            "cost": 7000,
+            "quantity": 0
+        }
+    }
+
 
 
     def build(self):
@@ -37,10 +62,18 @@ class MainApp(App):
         self.cash += self.income
         self.cashStr = 'Money:' + str(self.cash) + "\u20ac"
 
-    def incomeUp(self, cost, up):
-        if cost <= self.cash:
-            self.cash -= cost
-            self.income += up
+    def getProduct(self, identifier):
+        if self.Products[identifier]["cost"] <= self.cash:
+            self.cash -= self.Products[identifier]["cost"]
+            self.Products[identifier]["quantity"] += 1
+
+
+
+
+
+
+
+
 
 
 
